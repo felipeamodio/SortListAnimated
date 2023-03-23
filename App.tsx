1e15fcc72e066273e4a4fac010b3c343e6971d19
Felipe Alves <felipeaamodio@gmail.com>
2023-03-23 20:32:58 -0300

    installed libs

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import {Home} from './src/screens/Home';
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={{flex: 1}}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent 
       />
       <Home />
-    </>
+    </GestureHandlerRootView>
   );
 }
